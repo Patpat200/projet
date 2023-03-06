e004a5f68352d26bbf7c7ff46d0a8704d1d8f163
Patpat200 <adilelb2007@gmail.com>
2023-03-06 18:56:01 +0100

    création d'un js

---
+++ script.js
@@ -0,0 +1,26 @@
+// 2eme methode
+
+
+// const btn1 = document.getElementById("btn1")
+// const btn2 = document.getElementById("btn2")
+// const btn3 = document.getElementById("btn3")
+// const btn4 = document.getElementById("btn4")
+// const btn5 = document.getElementById("btn5")
+// const btn6 = document.getElementById("btn6")
+// const btn7 = document.getElementById("btn7")
+
+btn1.onclick = () => document.body.style.backgroundColor = "blue";
+btn2.onclick = () => document.body.style.backgroundColor = "red";
+btn3.onclick = () => document.body.style.backgroundColor = "green";
+btn4.onclick = () => document.body.style.backgroundColor = "purple";
+btn5.onclick = () => document.body.style.backgroundColor = "yellow";
+btn6.onclick = () => document.body.style.backgroundColor = "white";
+btn7.onclick = () => document.body.style.backgroundColor = "black";
+
+
+var compteur = parseInt(document.getElementById("compteur").innerHTML);
+function incrementerCompteur() {
+    compteur += 1;
+    document.getElementById("compteur").innerHTML = compteur;
+  }
+  
